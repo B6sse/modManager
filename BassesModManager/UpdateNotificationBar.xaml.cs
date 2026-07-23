b60b3afa1f9e089ac9e2aaf8d6d55547e51e8709
B6sse <bastiansenj@gmail.com>
2026-07-24 01:09:49 +0200

    fix: added sound effects to app update buttons

--- BassesModManager/UpdateNotificationBar.xaml.cs
+++ BassesModManager/UpdateNotificationBar.xaml.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace BassesModManager
 {
@@ -10,15 +13,55 @@ namespace BassesModManager
     /// </summary>
     public partial class UpdateNotificationBar : UserControl
     {
+        private MediaPlayer _hoverPlayer;
+        private MediaPlayer _clickPlayer;
+
         public UpdateNotificationBar()
         {
             InitializeComponent();
+            PreloadSounds();
             Loaded += (s, e) =>
             {
                 UpdateService.StateChanged += OnUpdateStateChanged;
                 Refresh();
             };
             Unloaded += (s, e) => UpdateService.StateChanged -= OnUpdateStateChanged;
+        }
+
+        private void PreloadSounds()
+        {
+            try
+            {
+                var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+                var hoverPath = Path.Combine(baseDir, "Assets", "Sounds", "hover.mp3");
+                var clickPath = Path.Combine(baseDir, "Assets", "Sounds", "click.mp3");
+                if (File.Exists(hoverPath))
+                {
+                    _hoverPlayer = new MediaPlayer();
+                    _hoverPlayer.Volume = 0.2;
+                    _hoverPlayer.Open(new Uri(hoverPath, UriKind.Absolute));
+                }
+                if (File.Exists(clickPath))
+                {
+                    _clickPlayer = new MediaPlayer();
+                    _clickPlayer.Volume = 0.2;
+                    _clickPlayer.Open(new Uri(clickPath, UriKind.Absolute));
+                }
+            }
+            catch { /* ignore preload errors */ }
+        }
+
+        private void PlayHoverSound(object sender, MouseEventArgs e) => PlayPreloaded(_hoverPlayer);
+
+        private static void PlayPreloaded(MediaPlayer player)
+        {
+            if (player == null) return;
+            try
+            {
+                player.Position = TimeSpan.Zero;
+                player.Play();
+            }
+            catch { /* ignore playback errors */ }
         }
 
         private void OnUpdateStateChanged(object sender, EventArgs e) => Dispatcher.Invoke(Refresh);
@@ -32,11 +75,16 @@ namespace BassesModManager
 
         private void UpdateNowButton_Click(object sender, RoutedEventArgs e)
         {
+            PlayPreloaded(_clickPlayer);
             UpdateNowButton.IsEnabled = false;
             UpdateService.ApplyUpdateAndRestart();
             UpdateNowButton.IsEnabled = true;
         }
 
-        private void LaterButton_Click(object sender, RoutedEventArgs e) => UpdateService.Dismiss();
+        private void LaterButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayPreloaded(_clickPlayer);
+            UpdateService.Dismiss();
+        }
     }
 }
